43e25f08fde07ac29d4b9c1710549f8e2ff7af11
Simon George <simon.george@zen.co.uk>
2015-11-23 18:09:08 +0000

    Added xunit tests

---
+++ Countdown.Tests/Tests.fs
@@ -0,0 +1,17 @@
+﻿module CountdownTests
+
+open Xunit
+open CountdownProblem
+
+[<Fact>]
+let ``Optimal method should find a valid solution`` () =
+
+    // Use the most optimal solution finder
+    let solutions = solutions'' [1; 3; 7; 10; 25; 50] 765
+
+    // One solution should be (25 - 10) * (1 + 50)
+    let oneSolution = App (Mul, App (Sub,(Val 25),(Val 10)), App (Add,(Val 1),(Val 50)) )
+
+    solutions 
+    |> List.contains oneSolution 
+    |> Assert.True
